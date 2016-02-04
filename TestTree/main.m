45ed3189264b2b0376481715d0b919cdd6520d6c
Wei <weizhang.zhang1988@gmail.com>
2016-02-04 13:09:15 -0500

    Initial Commit

---
+++ TestTree/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestTree
+//
+//  Created by victor zhang on 2/4/16.
+//  Copyright © 2016 victor zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
